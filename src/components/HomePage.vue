ec8446f62258a330c70020eff8b03b50241fee3b
ryan <1184518756@qq.com>
2019-08-29 11:03:33 +0800

    home page init

---
+++ src/components/HomePage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <van-nav-bar
+                :title="activePage.label"
+                fixed
+        ></van-nav-bar>
+        <div>
+            <component :is="activePage.api"></component>
+        </div>
+        <van-tabbar v-model="activeTabIndex">
+            <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+            <van-tabbar-item icon="search">标签</van-tabbar-item>
+            <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+            <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import {NavBar} from 'vant';
+    import {Tabbar, TabbarItem} from 'vant';
+    import HomeIndex from "@/components/home-pages/HomeIndex";
+    import HelloWorld from "@/components/HelloWorld";
+
+    Vue.use(NavBar).use(Tabbar).use(TabbarItem);
+
+    export default {
+        name: "HomePage",
+        data() {
+            return {
+                activeTabIndex: 0,
+                pages: [
+                    {title: '主页', label: '主页', api: HomeIndex, icon: 'home-o'},
+                    {title: '搜索', label: '搜索', api: HelloWorld, icon: 'search'},
+                    {title: '朋友', label: '朋友', api: HomeIndex, icon: 'friends-o'},
+                    {title: '设置', label: '设置', api: HomeIndex, icon: 'setting-o'},
+                ],
+            }
+        },
+        computed: {
+            activePage() {
+                return this.pages[this.activeTabIndex] || {};
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/home-pages/HomeIndex.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <van-grid>
+            <van-grid-item
+                    v-for="value in 4"
+                    :key="value"
+                    icon="photo-o"
+                    text="文字"></van-grid-item>
+        </van-grid>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import { Grid, GridItem } from 'vant';
+
+    Vue.use(Grid).use(GridItem);
+    export default {
+        name: "HomeIndex"
+    }
+</script>
+
+<style scoped>
+
+</style>
