62884eed575d48e23562f73392da0c9620b816fa
ryan <1184518756@qq.com>
2019-08-30 12:16:07 +0800

    exam completed step1

--- src/components/exam/DoExamPage.vue
+++ src/components/exam/DoExamPage.vue
@@ -4,19 +4,21 @@
                 :title="examInfo.title"
                 fixed
                 left-arrow
+                @click-left="$router.back()"
         ></van-nav-bar>
         <div v-for="(question, i) in questionsInfo" :key="i" v-if="i===questionNum">
-            <van-panel :title="getQuestionTitle(question)">
+            <van-panel :title="getQuestionTitle(question)" :desc="getQuestionDesc(question)">
                 <div v-if="question.isMulti">
                     <van-checkbox-group v-model="question.answer.multi.apis">
                         <van-cell-group>
                             <van-cell v-for="(option, oi) in question.answerOptions"
-                                      clickable @click="toggleAnswer(i, [option.api])"
+                                      clickable @click="toggleAnswer(i, option.api)"
                                       :key="oi">
                                 <template slot="title">
-                                    <span :class="{'right-answer':option.isRight&&question.answer.isAnswered,'error-answer':!option.isRight&&question.answer.isAnswered,'custom-title':true}">{{letterIndex[oi]+': '+option.describe}}</span>
+                                    <span :class="{'right-answer':option.isRight&&question.answer.isAnswered,'error-answer':!option.isRight&&question.answer.isAnswered,'custom-title':true}">{{letterIndex[oi]+'. '+option.describe}}</span>
                                 </template>
-                                <van-checkbox slot="right-icon" :name="option.api"></van-checkbox>
+                                <van-checkbox slot="right-icon" :name="option.api"
+                                              :checked-color="getOptionColor(question.answer.results,option)"></van-checkbox>
                             </van-cell>
                         </van-cell-group>
                     </van-checkbox-group>
@@ -28,10 +30,10 @@
                                       clickable @click="setAnswer(i, [option.api])"
                                       :key="oi">
                                 <template slot="title">
-                                    <span :class="{'right-answer':option.isRight&&question.answer.isAnswered,'error-answer':!option.isRight&&question.answer.isAnswered,'custom-title':true}">{{letterIndex[oi]+': '+option.describe}}</span>
+                                    <span :class="{'right-answer':option.isRight&&question.answer.isAnswered&&isShowResult,'error-answer':!option.isRight&&question.answer.isAnswered&&isShowResult,'custom-title':true}">{{letterIndex[oi]+'. '+option.describe}}</span>
                                 </template>
                                 <van-radio slot="right-icon" :name="option.api"
-                                           :checked-color="question.answer.results.map(res=>res.api).includes(option.api)&&option.isRight?'#44af11':'#cd0000'"></van-radio>
+                                           :checked-color="getOptionColor(question.answer.results,option)"></van-radio>
                             </van-cell>
                         </van-cell-group>
                     </van-radio-group>
@@ -40,10 +42,20 @@
                     <div class="text-align-right">
                         <van-button size="small" v-if="i>0" @click="turnTo(i-1)">上一题</van-button>
                         <van-button size="small" type="info" v-if="i<totalNum-1" @click="turnTo(i+1)">下一题</van-button>
-                        <van-button size="small" type="info" v-if="i===totalNum-1" @click="submit">提交</van-button>
+                        <van-button size="small" type="primary" @click="submit">提交</van-button>
                     </div>
                 </div>
             </van-panel>
+        </div>
+        <div class="exam-m-top-small">
+            <div class="exam-p-around-small">
+                <van-row>
+                    <van-col span="4" v-for="(question,i) in questionsInfo" :key="i">
+                        <div class="exam-circle exam-done" @click="turnTo(i)"
+                             :class="{'exam-right':question.answer.isPass&&isShowResult,'exam-error':question.answer.isMistake&&isShowResult,'exam-current': questionNum === i}">{{i+1}}</div>
+                    </van-col>
+                </van-row>
+            </div>
         </div>
     </div>
 </template>
@@ -56,7 +68,12 @@
     import {Cell, CellGroup} from 'vant';
     import {RadioGroup, Radio} from 'vant';
     import {Checkbox, CheckboxGroup} from 'vant';
+    import {Dialog} from 'vant';
+    import { Row, Col } from 'vant';
 
+    Vue.use(Row).use(Col);
+    // 全局注册
+    Vue.use(Dialog);
     Vue.use(Checkbox).use(CheckboxGroup);
     Vue.use(RadioGroup);
     Vue.use(Radio);
@@ -87,8 +104,9 @@
             return {
                 answers: [],
                 questionNum: 0,
-                isShowResult: true,
-                letterIndex: [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+                isShowResult: false,
+                letterIndex: [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'],
+                examInfo: {}
             }
         },
         computed: {
@@ -118,6 +136,20 @@
             getQuestionTitle(question) {
                 return [`(${this.questionNum + 1}/${this.totalNum})`, question.describe].join(' ');
             },
+            getQuestionDesc(question) {
+                let result = '';
+                if (!question.required) {
+                    result += '（可选）'
+                }
+                if (question.type === 'option') {
+                    if (question.isMulti) {
+                        result += '多选题'
+                    } else {
+                        result += '单选题'
+                    }
+                }
+                return result;
+            },
             /**
              * @param i
              * @param api {Array}
@@ -131,23 +163,20 @@
                     }
                     return answer;
                 });
-                if (this.isShowResult) {
-                    this.checkResult(i);
-                }
             },
             toggleAnswer(i, api) {
-                console.log(this.answers[i]);
                 let apis = this.answers[i].multi.apis;
                 if (apis.includes(api)) {
                     apis = apis.filter(a => a !== api);
                 } else {
                     apis.push(api);
+                    apis = [...apis];
                 }
                 this.setAnswer(i, apis);
             },
             checkResult(i) {
                 this.answers = [...this.answers].map((answer, ai) => {
-                    if (i !== ai) {
+                    if (i !== ai && i != null) {
                         return answer;
                     }
                     let question = this.questions[ai];
@@ -155,8 +184,8 @@
                     answer.results.forEach(res => {
                         res.isMistake = !rightAnswers.includes(res.api);
                     });
-                    answer.isAnswered = true;
-                    answer.isMistake = !!answer.results.find(res => res.isMistake);
+                    answer.isAnswered = answer.results.filter(res => res.api).length > 0;
+                    answer.isMistake = answer.results.length !== rightAnswers.length || !!answer.results.find(res => res.isMistake);
                     answer.isPass = answer.isAnswered && !answer.isMistake;
                     return answer;
                 })
@@ -165,24 +194,45 @@
                 this.questionNum = i;
             },
             submit() {
+                this.checkResult();
                 let answeredNum = this.questionsInfo.filter(q => q.answer.isAnswered).length;
                 if (answeredNum !== this.totalNum) {
-                    alert('题目还没有答完：' + `${answeredNum}/${this.totalNum}`);
+                    Dialog.alert({message: '题目还没有答完：' + `${answeredNum}/${this.totalNum}`});
                     return;
                 }
                 let rightNum = this.questionsInfo.filter(q => q.answer.isPass).length;
                 let rightRate = (rightNum / this.totalNum) * 100 + '%';
-                alert('答题结束，成绩如下：' + `正确：${rightNum}(总共：${this.totalNum})(正确率：${rightRate})`);
+                this.isShowResult = true;
+                Dialog.alert({message: '答题结束，成绩如下：' + `正确：${rightNum}(总共：${this.totalNum})(正确率：${rightRate})`});
+            },
+            adaptExamConfig() {
+                let examInfo = {...defaultExamInfo, ...this.examConfig};
+                examInfo.questions = examInfo.questions.map(q => ({...defaultQuestionInfo, ...q}));
+                this.examInfo = examInfo;
+            },
+            getOptionColor(results, option) {
+                if (this.isShowResult) {
+                    return results.map(res => res.api).includes(option.api) && option.isRight ? '#44af11' : '#cd0000'
+                }
+                return '#1989fa';
             }
         },
         props: {
-            examInfo: {
+            examConfig: {
                 type: Object,
                 default() {
                     return defaultExamInfo;
                 }
             }
         },
+        watch: {
+            examConfig() {
+                this.adaptExamConfig();
+            }
+        },
+        created() {
+            this.adaptExamConfig();
+        }
     }
 </script>
 
@@ -198,4 +248,42 @@
     span.custom-title {
         word-break: break-all;
     }
+
+    .exam-circle{
+        height: 2rem;
+        width: 2rem;
+        border: 1px solid #1989fa;
+        border-radius: 50%;
+        line-height: 2rem;
+        text-align: center;
+        color: #1989fa;
+        cursor: pointer;
+        margin-bottom: .5rem;
+    }
+
+    .exam-circle.exam-error{
+        color: #cd0000;
+        border-color: #cd0000;
+    }
+
+    .exam-circle.exam-right{
+        color: #44af11;
+        border-color: #44af11;
+    }
+
+    .exam-circle.exam-current{
+        background-color: #1989fa;
+        color: white;
+    }
+
+    .exam-circle.exam-error.exam-current{
+        background-color: #cd0000;
+        color: white;
+    }
+
+    .exam-circle.exam-right.exam-current{
+        background-color: #44af11;
+        color: white;
+    }
+
 </style>
--- src/components/home-pages/HomeIndex.vue
+++ src/components/home-pages/HomeIndex.vue
@@ -2,69 +2,74 @@
     <div>
         <van-grid>
             <van-grid-item
-                    v-for="value in 4"
+                    v-for="value in 1"
                     :key="value"
                     icon="notes-o"
                     clickable
                     @click="doExam"
-                    text="文字"></van-grid-item>
+                    text="开始答题"></van-grid-item>
         </van-grid>
+        <van-popup v-model="show">
+            <div class="exam-import-box">
+                <van-cell-group>
+                    <van-field
+                            v-model="questionLib"
+                            label="题库"
+                            type="textarea"
+                            placeholder="请输入题库"
+                            rows="10"></van-field>
+                </van-cell-group>
+                <van-button type="info" size="small" block @click="importLib">导入题库</van-button>
+            </div>
+        </van-popup>
     </div>
 </template>
 
 <script>
     import Vue from 'vue';
     import {Grid, GridItem} from 'vant';
+    import {Popup} from 'vant';
+    import {Button} from 'vant';
+    import {Field} from 'vant';
+    import {Cell, CellGroup} from 'vant';
 
+
+    Vue.use(Field).use(Cell).use(CellGroup);
+    Vue.use(Button);
+    Vue.use(Popup);
     Vue.use(Grid).use(GridItem);
     export default {
         name: "HomeIndex",
+        data() {
+            return {
+                show: false,
+                questionLib: ''
+            }
+        },
         methods: {
             doExam() {
-                this.$router.push({
-                    name: 'DoExamPage',
-                    params: {
-                        examInfo: {
-                            questions:[
-                                {
-                                    describe: 'Do you like me?',
-                                    answerOptions: [
-                                        {api: 'A', describe: 'Yes', isRight: true},
-                                        {api: 'B', describe: 'No'}
-                                    ]
-                                },
-                                {
-                                    describe: 'Do you hate me?',
-                                    answerOptions: [
-                                        {api: 'A', describe: 'Yes'},
-                                        {api: 'B', describe: 'No', isRight: true}
-                                    ]
-                                },
-                                {
-                                    describe: 'Which one is right for me?',
-                                    answerOptions: [
-                                        {api: 'A', describe: 'My hometown is DeYang', isRight: true},
-                                        {api: 'B', describe: 'My age is 18 year old', isRight: true},
-                                        {api: 'C', describe: 'I am a girl'},
-                                        {api: 'D', describe: 'I am not smart'}
-                                    ]
-                                },
-                                {
-                                    describe: 'Do you want me?',
-                                    answerOptions: [
-                                        {api: 'A', describe: 'Yes', isRight: true},
-                                        {api: 'B', describe: 'No'}
-                                    ]
-                                }
-                            ]
+                this.show = true;
+            },
+            importLib() {
+                try {
+                    let lib = JSON.parse(this.questionLib);
+                    this.$router.push({
+                        name: 'DoExamPage',
+                        params: {
+                            examConfig: lib
                         }
-                    }
-                })
+                    })
+                } catch (e) {
+                    alert('题库格式错误')
+                }
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .exam-import-box {
+        width: 20rem;
+        padding: 1rem;
+    }
 </style>
